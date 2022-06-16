e1c4ab5522f706121da024cb1398228e38e26b26
shootinstar000 <93067533+shootinstar000@users.noreply.github.com>
2022-06-16 09:18:02 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,31 +1,19 @@
-BOT_TOKEN = "1629027959:AAEaTw4s2qaAL3mYP3fQRnE"
+BOT_TOKEN = "5483954570:AAG-JAdiNdpiqk0lp6vFF6Jb8-9EsU4gfAw"
 RESULTS_COUNT = 4  # NOTE Number of results to show, 4 is better
-SUDO_CHATS_ID = [-1001485393652, -1005456463651]
+SUDO_CHATS_ID = [5565270008 ]
 
 
 DRIVE_NAME = [
-    "Root",  # folder 1 name
-    "Cartoon",  # folder 2 name
-    "Course",  # folder 3 name
-    "Movies",  # ....
-    "Series",  # ......
-    "Others"  # and soo onnnn folder n names
+    "Root"# folder 1 name
+
 ]
 
 DRIVE_ID = [
-    "1B9A3QqQqF31IuW2om3Qhr-wkiVLloxw8",  # folder 1 id
-    "12wNJTjNnR-CNBOTnLHqe-1vqFvCRLecn",  # folder 2 id
-    "11nZcObsJJHojHYg43dBS0_eVvJrSD7Nf",  # and so onn... folder n id
-    "10_hTMK8HE8k144wOTth_3x1hC2kZL-LR",
-    "1-oTctBpyFcydDNiptLL09Enwte0dClCq",
-    "1B9A3QqQqF31IuW2om3Qhr-wkiVLloxw8"
+    "0AHMOAv54xMnAUk9PVA"  # folder 1 id
+
 ]
 
 INDEX_URL = [
-    "https://dl.null.tech/0:",  # folder 1 index link
-    "https://dl.null.tech/0:/Cartoon",  # folder 2 index link
-    "https://dl.null.tech/0:/Course",  # and soo on folder n link
-    "https://dl.null.tech/0:/MOVIES",
-    "https://dl.null.tech/0:/Series",
-    "https://dl.null.tech/0:/Roms"
+    "https://dl.null.tech/0:"# folder 1 index link
+
 ]
